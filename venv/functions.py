ad4d99351071ce33fdffb6a30a60c5d51adab5a4
raj <rp5804@rit.edu>
2021-01-12 21:36:48 -0500

    commit1

---
+++ venv/functions.py
@@ -0,0 +1,7 @@
+from util import create_device
+
+from tabulate import tabulate
+
+if __name__ == '__main__':
+    devices = create_device(num_devices=3, num_subnets=3)
+    print('\n', tabulate(devices, headers='keys'))
